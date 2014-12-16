bc7378ef3cb72ff4206c27ea734b9056cf8ea754
Tiago Fischer <tiagofischer@gmail.com>
2014-12-16 20:55:53 -0200

    Revert "Version using forEach"
    This reverts commit 3fe92b6efa47685887376e3bd9b5473fe30aa5f1.

--- mymodule.js
+++ mymodule.js
@@ -5,13 +5,15 @@ module.exports = function(dir, ext, callback) {
 		if (err) {
 			return callback(err);
 		} else {
-			var files = [];
+			//var files = [];
 	        	//for (var i = 0; i < list.length; i++) {
-			list.forEach(function(elem) {
-			//var files = list.filter(function(elem) {
+			//list.forEach(function(elem) {
+			var files = list.filter(function(elem) {
 		                if (path.extname(elem).replace('.', '') == ext) {
-					files.push(elem);
+					//files.push(elem);
+					return true;
         	        	}
+				return false;
 		        });
 			return callback(null, files);
 		}
