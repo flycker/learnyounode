9874a792fa0b4e0ef7669ef7ce43719c34080d78
Tiago Fischer <tiagofischer@gmail.com>
2014-12-16 21:48:27 -0200

    Lack of creativity I guess

--- filterls.js
+++ filterls.js
@@ -1,6 +1,6 @@
 var my = require('./mymodule.js');
 if (typeof(process.argv[2]) == 'undefined' || typeof(process.argv[3]) == 'undefined') {
-	console.log('Lack of parameters!!');
+	console.log('Lack of parameters!!!');
 	return;
 }
 my(process.argv[2], process.argv[3], function(err, list) {
